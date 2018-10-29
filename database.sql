9b16e141f91f2b934873a2f4e0013efac3ef963f
theishah <ishans@usc.edu>
2018-10-28 19:04:16 -0700

    Updated Database Schma

--- database.sql
+++ database.sql
@@ -2,8 +2,8 @@ drop DATABASE if exists UserProfiles;
 CREATE DATABASE UserProfiles;
 USE UserProfiles;
 CREATE TABLE Users (
-    username VARCHAR(50) PRIMARY KEY NOT NULL, 
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) PRIMARY KEY NOT NULL, 
+    username VARCHAR(50) NOT NULL,
     password VARCHAR (200) NOT NULL
 );
 
